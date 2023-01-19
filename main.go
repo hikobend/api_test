40749872e70a10591fc6db8e2165121c84eb7f82
hikobend <hiko.bkend@gmail.com>
2023-01-19 21:48:39 +0900

    データ登録

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
@@ -25,10 +28,20 @@ func get(c *gin.Context) {
 }
 
 func create(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	var u User
 	if err := c.BindJSON(&u); err != nil {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "error"})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"msg": u})
+
+	insert, _ := db.Prepare("INSERT INTO User (name) VALUES (?)")
+
+	insert.Exec(u.Name)
 }
